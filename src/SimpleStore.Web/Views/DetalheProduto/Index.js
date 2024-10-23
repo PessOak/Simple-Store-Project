11ae0b470f333581ff3ef1a25223a086f3147a3e
Rafael <pessoa.carvalho@outlook.com>
2024-10-23 09:53:22 -0300

    modificacoes nas views

---
+++ src/SimpleStore.Web/Views/DetalheProduto/Index.js
@@ -0,0 +1,24 @@
+﻿function changeImage(element) {
+    // Pega o caminho da imagem clicada
+    const newSrc = element.src.replace("150", "600"); // Substitui a resolução da imagem pequena pela grande
+    // Define a nova imagem no elemento principal
+    document.getElementById('main-image').src = newSrc;
+}
+
+// Carrega a primeira imagem como principal ao carregar a página
+window.onload = function () {
+    const firstImage = document.getElementById('img1');
+    changeImage(firstImage);  // Faz com que a primeira imagem seja carregada como principal
+};
+
+function increaseQuantity() {
+    const quantityInput = document.getElementById('product-quantity');
+    quantityInput.value = parseInt(quantityInput.value) + 1;
+}
+
+function decreaseQuantity() {
+    const quantityInput = document.getElementById('product-quantity');
+    if (quantityInput.value > 1) {
+        quantityInput.value = parseInt(quantityInput.value) - 1;
+    }
+}
