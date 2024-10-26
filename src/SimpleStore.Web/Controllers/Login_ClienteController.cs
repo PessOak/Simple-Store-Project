56e1f146cf29ac63900d97f26b48a6daa9ce707d
chainarthur <arthur.chain@gmail.com>
2024-10-26 00:09:24 -0300

    Inserção das páginas login Cliente e Login Fornecedor no MVC

---
+++ src/SimpleStore.Web/Controllers/Login_ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleStore.Web.Models;
+using System.Diagnostics;
+
+namespace SimpleStore.Web.Controllers
+{
+    public class Login_ClienteController : Controller
+    {
+        private readonly ILogger<Login_ClienteController> _logger;
+
+        public Login_ClienteController(ILogger<Login_ClienteController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/SimpleStore.Web/Controllers/Login_FornecedorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleStore.Web.Models;
+using System.Diagnostics;
+
+namespace SimpleStore.Web.Controllers
+{
+    public class Login_FornecedorController : Controller
+    {
+        private readonly ILogger<Login_FornecedorController> _logger;
+
+        public Login_FornecedorController(ILogger<Login_FornecedorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
