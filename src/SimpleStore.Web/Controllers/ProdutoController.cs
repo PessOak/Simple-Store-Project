0f5e1e6e63db531ef76db7f87929a2be34c6ca7c
Rafael <pessoa.carvalho@outlook.com>
2024-11-07 09:57:47 -0300

    observação em produto controller

--- src/SimpleStore.Web/Controllers/ProdutoController.cs
+++ src/SimpleStore.Web/Controllers/ProdutoController.cs
@@ -28,6 +28,7 @@ public class ProdutoController : Controller
             return NotFound();
         }
 
+        //FAZER alteração aqui para redirecionar apenas quando pessoa não logada tentar adicionar o produto, não quando clicar em ver detalhes
         // Recuperar o CPF do usuário logado usando Claims (caso esteja utilizando login baseado em CPF)
         var cpfComp = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
 
