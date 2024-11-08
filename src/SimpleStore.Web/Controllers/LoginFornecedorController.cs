4ebaf43f8165c215c1c3ff3b0b014e8ef2f625f1
chainarthur <arthur.chain@gmail.com>
2024-11-08 16:17:00 -0300

    Inserção Página LoginFornecedor

--- src/SimpleStore.Web/Controllers/LoginFornecedorController.cs
+++ src/SimpleStore.Web/Controllers/LoginFornecedorController.cs
@@ -1,14 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using SimpleStore.Web.Models;
+using SimpleStore.Web.Services;
+using System.Security.Claims;
 
 namespace SimpleStore.Web.Controllers
 {
-    public class LoginFornecedorController(ILogger<LoginFornecedorController> logger) : Controller
+    public class LoginFornecedorController(LoginFornecedorService loginFornecedorService) : Controller
     {
-        private readonly ILogger<LoginFornecedorController> _logger = logger;
+        private readonly LoginFornecedorService _loginFornecedorService = loginFornecedorService;
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        //public IActionResult Login()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                var fornecedor = await _loginFornecedorService.ObterFornecedor(login.Email, login.Senha);
+                bool loginValido = _loginFornecedorService.ValidarLogin(fornecedor);
+
+                if (loginValido)
+                {
+                    // Pode adicionar propriedades para a sessão do usuário aqui
+                    var claims = new List<Claim>
+                    {
+                        new(ClaimTypes.Name, fornecedor.NomeForn),
+                        new(ClaimTypes.NameIdentifier, fornecedor.DocForn),
+                        new(ClaimTypes.Email, fornecedor.EmailForn)
+                    };
+
+                    var identity = new ClaimsIdentity(claims, "login");
+                    ClaimsPrincipal principal = new(identity);
+
+                    var props = new AuthenticationProperties
+                    {
+                        AllowRefresh = true,
+                        ExpiresUtc = DateTime.UtcNow.ToLocalTime().AddHours(12),
+                        IsPersistent = true,
+                    };
+
+                    await HttpContext.SignInAsync(principal, props);
+                    return RedirectToAction("Index", "Produto"); // Redireciona para a página desejada após o login
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Documento ou senha inválidos."); // Mensagem de erro
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index");
         }
 
     }
---
+++ src/SimpleStore.Web/Services/LoginFornecedorService.cs
@@ -0,0 +1,21 @@
+﻿using SimpleStore.Web.Models;
+using SimpleStore.Web.Repositories;
+
+namespace SimpleStore.Web.Services
+{
+    public class LoginFornecedorService(LoginFornecedorRepository loginfornecedorRepository)
+    {
+        private readonly LoginFornecedorRepository _loginfornecedorRepository = loginfornecedorRepository;
+
+        public async Task<Fornecedor> ObterFornecedor(string email, string senha)
+        {
+            return await _loginfornecedorRepository.ObterFornecedorPorEmailESenha(email, senha);
+        }
+
+        public bool ValidarLogin(Fornecedor fornecedor)
+        {
+            return fornecedor != null; // Retorna true se o comprador foi encontrado
+            // Se necessário adicionar mais validações aqui
+        }
+    }
+}
