5ca7d8f4e5d0bb44d3036f9f4334c6eb666f8439
Erasmo Alves <erasmoajr@gmail.com>
2024-11-12 13:35:26 -0300

    Atualiza CarrinhoController

--- src/SimpleStore.Web/Controllers/CarrinhoController.cs
+++ src/SimpleStore.Web/Controllers/CarrinhoController.cs
@@ -1,13 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
+using SimpleStore.Web.Models;
+using SimpleStore.Web.Services;
 
 namespace SimpleStore.Web.Controllers
 {
     public class CarrinhoController : Controller
     {
-        // Action para exibir o conteúdo do carrinho
-        public IActionResult Index()
+        private readonly CarrinhoService _carrinhoService;
+
+        public CarrinhoController(CarrinhoService carrinhoService)
         {
-            return View();
+            _carrinhoService = carrinhoService;
+        }
+
+        // Exibir o conteúdo do carrinho
+        public IActionResult Index(string cpfComprador)
+        {
+            var carrinho = _carrinhoService.ObterCarrinho(cpfComprador); // Obter o carrinho específico do comprador
+            return View(carrinho);
+        }
+
+        // Adicionar produto ao carrinho
+        [HttpPost]
+        public IActionResult AdicionarProduto(string cpfComprador, int idProduto, int quantidade)
+        {
+            _carrinhoService.AdicionarProduto(cpfComprador, idProduto, quantidade);
+            return RedirectToAction("Index", new { cpfComprador });
+        }
+
+        // Remover produto do carrinho
+        [HttpPost]
+        public IActionResult RemoverProduto(string cpfComprador, int idProduto)
+        {
+            _carrinhoService.RemoverProduto(cpfComprador, idProduto);
+            return RedirectToAction("Index", new { cpfComprador });
+        }
+
+        // Atualizar quantidade do produto no carrinho
+        [HttpPost]
+        public IActionResult AtualizarQuantidade(string cpfComprador, int idProduto, int quantidade)
+        {
+            _carrinhoService.AtualizarQuantidade(cpfComprador, idProduto, quantidade);
+            return RedirectToAction("Index", new { cpfComprador });
         }
     }
 }
