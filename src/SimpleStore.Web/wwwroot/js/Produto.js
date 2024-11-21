786538ba116c34e268977eb32f1a71c80736d8b4
Rafael <pessoa.carvalho@outlook.com>
2024-11-21 11:07:21 -0300

    criação da funcionalidade de pesquisa na página de produtos

---
+++ src/SimpleStore.Web/wwwroot/js/Produto.js
@@ -0,0 +1,22 @@
+﻿// Capturar o formulário de pesquisa e adicionar evento de envio
+document.getElementById('formPesquisa').addEventListener('submit', function (event) {
+    event.preventDefault(); // Evita o envio do formulário
+
+const termoPesquisa = document.getElementById('inputPesquisa').value.toLowerCase().trim();
+const produtos = document.querySelectorAll('.produto'); // Todos os produtos exibidos
+
+    produtos.forEach(produto => {
+        const nome = produto.getAttribute('data-nome').toLowerCase();
+
+// Verifica se o termo de pesquisa está presente no nome ou descrição
+if (nome.includes(termoPesquisa)) {
+    produto.style.display = 'block'; // Exibe o produto
+        } 
+
+else {
+    produto.style.display = 'none'; // Oculta o produto
+        }
+
+    });
+});
+
