03084b72ec1b8feb8dfda33e21d51fec115d6300
Rafael <pessoa.carvalho@outlook.com>
2024-11-24 11:57:41 -0300

    pop up adicionado após adição ao carrinho

---
+++ src/SimpleStore.Web/wwwroot/js/ProdutoCarrinho.js
@@ -0,0 +1,21 @@
+﻿document.getElementById('adicionar-carrinho').addEventListener('click', function (event) {
+    event.preventDefault(); // Previne o envio padrão do formulário
+
+    const form = document.getElementById('form-adicionar-carrinho');
+    const formData = new FormData(form);
+
+    fetch('/ProdutoCarrinho/AdicionarAoCarrinho', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => {
+            if (response.ok) {
+                alert('Produto adicionado ao carrinho!');
+                setTimeout(() => alertDiv.remove(), 3000);
+
+            } else {
+                alert('Erro ao adicionar o produto ao carrinho.');
+            }
+        })
+        .catch(error => console.error('Erro de rede:', error));
+});
