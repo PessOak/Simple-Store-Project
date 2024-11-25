ea6fa6c63ff0c8db56b5f93d858a4a032ba57a5d
Rafael <pessoa.carvalho@outlook.com>
2024-11-24 22:29:38 -0300

    novo carrinho service

---
+++ src/SimpleStore.Web/Services/CarrinhoService.cs
@@ -0,0 +1,44 @@
+﻿using SimpleStore.Web.Models;
+using SimpleStore.Web.Repositories;
+
+namespace SimpleStore.Web.Services
+{
+    public class CarrinhoService
+    {
+        private readonly CarrinhoRepository _carrinhoRepository;
+
+        public CarrinhoService(CarrinhoRepository carrinhoRepository)
+        {
+            _carrinhoRepository = carrinhoRepository;
+        }
+
+        public async Task<Carrinho> ObterCarrinhoOuCriarAsync(string cpfComp)
+        {
+            // Tenta obter o carrinho do usuário
+            var carrinho = await _carrinhoRepository.ObterCarrinhoPorCpfAsync(cpfComp);
+
+            // Se o carrinho não existir, cria um novo
+            if (carrinho == null)
+            {
+                carrinho = new Carrinho
+                {
+                    CpfComp = cpfComp,
+                    Produtos = new List<ProdutoCarrinho>()
+                };
+
+                // Cria o novo carrinho no banco de dados
+                await _carrinhoRepository.CriarCarrinhoAsync(carrinho);
+            }
+
+            return carrinho;
+        }
+
+
+        //remoção de produto do carrinho
+        public async Task<bool> RemoverProdutoDoCarrinhoAsync(string cpfComp, int idProduto)
+        {
+            // Chama o repositório para remover o produto do carrinho
+            return await _carrinhoRepository.RemoverProdutoDoCarrinhoAsync(cpfComp, idProduto);
+        }
+    }
+}
